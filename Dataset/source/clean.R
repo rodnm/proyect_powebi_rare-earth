3fbf5cafafd740c712c80a6b2a9b7294d991ff99
rodnm <rod_nm@outlook.com>
2024-07-22 00:57:40 -0500

    Update clean.R

--- Dataset/source/clean.R
+++ Dataset/source/clean.R
@@ -16,17 +16,7 @@ setwd(directorio)
 # cargar las librerias ----
 #######################################
 
-# library(tidyverse)
-# library(lubridate)
-# library(stringr)
-# library(ggplot2)
-# library(ggthemes)
-# library(readr)
-# library(gridExtra)
-# library(cowplot)
-# library(dplyr)
-
-# Instala los paquetes necesarios si no los tienes instalados
+# Instala los paquetes necesarios si hace falta
 if (!require(jsonlite)) install.packages("jsonlite")
 if (!require(dplyr)) install.packages("dplyr")
 if (!require(openxlsx)) install.packages("openxlsx")
@@ -36,51 +26,51 @@ library(jsonlite)
 library(dplyr)
 library(openxlsx)
 
-# Define el directorio de trabajo donde están los archivos JSON
+# Definir el directorio de trabajo donde están los archivos JSON
 directorio <- dirname(rstudioapi::getSourceEditorContext()$path) # path del directorio que contiene este archivo R
 setwd(directorio)
 
-# Obtén la lista de archivos JSON en la carpeta
+# Obtener la lista de archivos JSON en la carpeta
 archivos_json <- list.files(path = directorio, pattern = "*.json", full.names = TRUE)
 
-# Define el rango de años
+# Definir el rango de años
 anios <- 1995:2022
 
-# Verifica si la cantidad de archivos coincide con la cantidad de años
+# Verificar si la cantidad de archivos coincide con la cantidad de años
 if (length(archivos_json) != length(anios)) {
   stop("El número de archivos JSON no coincide con el número de años.")
 }
 
-# Importa cada archivo JSON, añade la columna Year y almacena en una lista
+# Importar cada archivo JSON, añade la columna Year y almacena en una lista
 lista_dataframes <- lapply(seq_along(archivos_json), function(i) {
-  # Importa el archivo JSON
+  # Importar el archivo JSON
   df <- fromJSON(archivos_json[i])
   
-  # Convierte los nombres de las columnas a minúsculas
+  # Convertir los nombres de las columnas a minúsculas
   colnames(df) <- tolower(colnames(df))
   
-  # Añade la columna Year
+  # Añadir la columna Year
   df <- mutate(df, year = anios[i])
   
   return(df)
 })
 
-# Combina todos los dataframes en uno solo
+# Combinar todos los dataframes en uno solo
 df_combinado <- bind_rows(lista_dataframes)
 
-# Define el nombre del archivo de salida en Excel y CSV
+# Definir el nombre del archivo de salida en Excel y CSV
 archivo_excel <- "net_trade_data.xlsx"
 archivo_csv <- "net_trade_data.csv"
 
-# Crea un nuevo workbook y añade el dataframe combinado con la hoja nombrada "net_trade"
+# Crear un nuevo workbook y añade el dataframe combinado con la hoja nombrada "net_trade"
 wb <- createWorkbook()
 addWorksheet(wb, "net_trade")
 writeData(wb, "net_trade", df_combinado)
 
-# Guarda el workbook en un archivo
+# Guardar el workbook en un archivo
 saveWorkbook(wb, archivo_excel, overwrite = TRUE)
 
-# Exporta el dataframe combinado como CSV separado por "|"
+# Exportar el dataframe combinado como CSV separado por "|"
 write.csv(df_combinado, archivo_csv, row.names = FALSE, sep = "|")
 # readr::write_excel_csv2(df_combinado, archivo_csv, row.names = FALSE, delim = "|")
 
